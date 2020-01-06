21f0a6e39e81323655aef71c1492e7bf495f3217
Abigail MARIANO <abigail.mariano@value-exch.com>
2020-01-06 17:07:20 +0800

    Some modifications..

--- www/testplugin.js
+++ www/testplugin.js
@@ -1,5 +1,13 @@
-window.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "Echo", "echo", [str]);
-}
+// window.echo = function(str, callback) {
+//     cordova.exec(callback, function(err) {
+//         callback('Nothing to echo.');
+//     }, "Echo", "echo", [str]);
+// }
+
+var exec = require('cordova/exec');
+
+module.exports = {
+    echo: function(message, onSuccess, onError) {
+        exec(onSuccess, onError, "Echo", "echo", [message]);
+    }
+};
